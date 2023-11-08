7ef852ad066f0d5af0e02e967e0fdbe5d96a3909
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-11-08 12:38:23 +0530

    hoja

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Dropdown } from '@mui/base/Dropdown';
+import { MenuButton } from '@mui/base/MenuButton';
+import { Menu } from '@mui/base/Menu';
+import { MenuItem } from '@mui/base/MenuItem';
+
 
 function App() {
   return (
     <div className="App">
+      <Dropdown>
+        <MenuButton>My account</MenuButton>
+        <Menu>
+          <MenuItem >Profile</MenuItem>
+          <MenuItem >
+            Language settings
+          </MenuItem>
+          <MenuItem >Log out</MenuItem>
+        </Menu>
+      </Dropdown>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
