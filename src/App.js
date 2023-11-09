80ea518356bd92cc24f130b62b575f0fbba2e42a
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-11-09 18:48:10 +0530

    hoja

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-import { Dropdown } from '@mui/base/Dropdown';
-import { MenuButton } from '@mui/base/MenuButton';
-import { Menu } from '@mui/base/Menu';
-import { MenuItem } from '@mui/base/MenuItem';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-    <div className="App">
-      <Dropdown>
-        <MenuButton>My account</MenuButton>
-        <Menu>
-          <MenuItem >Profile</MenuItem>
-          <MenuItem >
-            Language settings
-          </MenuItem>
-          <MenuItem >Log out</MenuItem>
-        </Menu>
-      </Dropdown>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React gaurav
-        </a>
-      </header>
+    <div style={{backgroundColor:'#648aab'}}>
+      <div className="dropdown">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item dropdown">
+          <button class="btn btn-secondary dropdown-toggle" style={{padding:'2px',backgroundColor:'#fffcfca6',color:'black',width:'120px'}} type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <b>Display</b>
+          </button>
+          <ul className="dropdown-menu" style={{width : '280px'}}>
+            <li style={{display : 'flex'}}><a className="dropdown-item" style={{width:'150px'}} href="#">Grouping</a>
+                <div className="form-group col-md-4" style={{width:'120px'}}>
+                  <select className="form-control" style={{padding:'2px'}}>
+                    <option value="Status">Status</option>
+                    <option value="User">User</option>
+                    <option value="Priority">Priority</option>
+                  </select>
+                </div>
+            </li>
+            <li style={{display : 'flex'}}><a className="dropdown-item" style={{width:'150px'}} href="#">Ordering</a>
+                <div className="form-group col-md-4" style={{width:'120px'}}>
+                  <select className="form-control" style={{padding:'2px'}}>
+                    <option value="Priority">Priority</option>
+                    <option value="Title">Title</option>
+                  </select>
+                </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      </div>
     </div>
   );
 }
